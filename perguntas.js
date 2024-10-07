796b47e01ce70ef6698bca24bdacc1a15d578222
chimpanzeburro <163125580+chimpanzeburro@users.noreply.github.com>
2024-10-07 11:15:30 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,37 @@
 
+criaCartao(
+    'Brasil' ,
+    'Quem descobriu o Brasil' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
